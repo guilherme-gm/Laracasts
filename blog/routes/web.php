29a07f6a9c79ee5817c383ff99219bb7528522a9
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2018-03-23 18:19:49 -0300

    Episode 6 - Working with Query builder

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -11,14 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    $tasks = [
-        'Go to store',
-        'task1',
-        'task2'
-    ];
+Route::get('/tasks', function () {
+    $tasks = DB::table('tasks')->get();
 
-    return view('welcome', compact('tasks'));
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function($id) {
+    $task = DB::table('tasks')->find($id);
+
+    return view('tasks.show', compact('task'));
 });
 
 Route::get('/about', function() {

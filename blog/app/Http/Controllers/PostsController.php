bbe4df455e23d1dcaa386f40eaf0c1a6d81e905d
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2018-03-24 00:27:00 -0300

    Episode 10 - Layouts and Structures

---
+++ blog/app/Http/Controllers/PostsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        return view('posts.index');
+    }
+
+    public function show(Post $post1)
+    {
+        return view('posts.show');
+    }
+}
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -13,6 +13,10 @@
 
 use App\Task;
 
+Route::get('/', 'PostsController@index');
+
+Route::get('/posts/{post}', 'PostsController@show');
+
 Route::get('/tasks', 'TaskController@index');
 
 Route::get('/tasks/{task}', 'TaskController@show');

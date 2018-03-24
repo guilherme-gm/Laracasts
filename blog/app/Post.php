5b35fecde6738786a6c71e787052430b87211619
Guilherme G. Menaldo <guilherme.menaldo@outlook.com>
2018-03-24 12:13:43 -0300

    Episode 15: Eloquent Relationships and Comments

--- blog/app/Post.php
+++ blog/app/Post.php
@@ -5,9 +5,8 @@ namespace App;
 class Post extends Model
 {
 
-    // Can be mass assigned
-    //protected $fillable = ['title', 'body'];
-    // Everything except this can be mass assigned
-    //protected $guarded = ['user_id'];
-    //
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -1,12 +1,29 @@
-@extends('layouts.master')
-
-@section('content')
+@extends('layouts.master') @section('content')
 
 <div class="col-sm-8 blog-main">
     <h1>{{ $post->title }}</h1>
     <p>
         {{ $post->body }}
     </p>
+
+    <hr />
+
+
+    <div class="comments">
+        <ul class="list-group">
+
+
+            @foreach($post->comments as $comment)
+            <li class="list-group-item">
+                <strong>
+                    {{ $comment->created_at->diffForHumans() }}: &nbsp;
+                </strong>
+                    {{ $comment->body }}
+                
+            </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
 
 @endsection
